56df3dcf2831f98acf687caec105064a956ffb1e
Sapayth-Lab <sapayth@users.noreply.github.com>
2019-08-25 15:28:03 +0600

    initial commit

---
+++ application/views/index.php
@@ -0,0 +1,14 @@
+<?php include("header.php"); ?>
+  <?php include("template-parts/header-admin.php"); ?>
+  <!-- Left side column. contains the logo and sidebar -->
+  <?php include("template-parts/sidebar-left-admin.php"); ?>
+
+  <!-- Content Wrapper. Contains page content -->
+  <?php include("placeholder-admin.php"); ?>
+  <!-- /.content-wrapper -->
+  <?php include("footer-copyright.php"); ?>
+
+  <!-- Control Sidebar -->
+  <?php include("template-parts/sidebar-control-admin.php"); ?>
+  <!-- /.control-sidebar -->
+<?php include("footer.php"); ?>
